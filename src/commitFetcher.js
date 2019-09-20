c1ecb8d7637a8a058acd50ace932dfb68e51e255
Alex <lex.2418@gmail.com>
2019-09-20 15:29:41 -0500

    fix: commit fetcher

--- src/commitFetcher.js
+++ src/commitFetcher.js
@@ -1,20 +1,16 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
 async function commitFetcher(version) {
   try {
     const { stdout, stderr } = await exec(`git log v${version}..HEAD --oneline`);
 
-    if (stderr) {
-      console.error(`error: ${stderr}`);
-    }
+    if (stderr) { console.error(`error: ${stderr}`) }
 
     return stdout.split('\n')
   } catch (e) {
-    throw new Error('\n\nThe version might be invalid or withot git tag\n\n')
+    throw new Error(`\nThe version might be invalid, or the tag v${version} is missing.\n`)
   }
-
 }
 
 module.exports = commitFetcher
