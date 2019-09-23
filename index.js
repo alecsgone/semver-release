b2b09e68aff1739164f627589e8f5b75079d6639
Alex <lex.2418@gmail.com>
2019-09-23 14:47:04 -0500

    rebuild main module file

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
 #!/usr/bin/env node
-const { spawnSync } = require('child_process')
+
 const semver = require('semver')
+const { version } = require(process.cwd() + '/package.json')
+const versionCalculator = require('./src/versionCalculator')
+const commitFetcher = require('./src/commitFetcher')
 
-const semVersions = ['patch', 'minor', 'major']
-const versions = ['fix', 'feat']
-const cmd = 'git'
-let range = 0
-
-function calculateRange(word) {
-  const currentRange = versions.indexOf(word)
-  range = range > currentRange ? range : currentRange
-}
-
-const lastVersion = require(process.cwd() + '/package.json').version
-const commits = spawnSync(cmd, `log v${lastVersion}..HEAD --oneline`.split(' '))
-const conventionalCommits = commits.output.map(commit => {
+async function main() {
   try {
-    return commit.toString().split('\n').filter(Boolean)
-      .map(splittedCommit => {
-        console.log(splittedCommit);
-        try {
-          const withoutCommit = splittedCommit.replace(/^\S+\s/, '')
-          const conventionalWord = withoutCommit.match(/[a-zA-Z]+\b/)[0]
+    const commits = await commitFetcher(version)
+    const nextBump = versionCalculator(commits)
 
-          calculateRange(conventionalWord)
-
-          if (versions.indexOf(conventionalWord) > -1) {
-            return splittedCommit
-          }
-        } catch (e) {
-          console.log('error for:', splittedCommit);
-        }
-      }).filter(Boolean)
+    if (nextBump) {
+      console.log(nextBump, semver.inc(version, nextBump))
+      console.log('attempt to release!');
+      // const npmVersion = spawnSync('npm', `version ${semVersions[range]}`.split(' '))
+      // console.log('createing with npm', npmVersion.stdout.toString())
+    } else {
+      console.log('no versions to bump');
+    }
   } catch (e) {
+    console.error('impossible to bump');
     console.log(e);
-    return ''
   }
-})
-.filter(item => item.length)
-
-console.log(conventionalCommits);
-
-if (conventionalCommits.length) {
-  console.log(lastVersion, semVersions[range], semver.inc(lastVersion, semVersions[range]))
-  console.log('attempt to release!');
-  const npmVersion = spawnSync('npm', `version ${semVersions[range]}`.split(' '))
-  console.log('createing with npm', npmVersion.stdout.toString())
-} else {
-  console.log('no versions to bump');
 }
+
+main()
